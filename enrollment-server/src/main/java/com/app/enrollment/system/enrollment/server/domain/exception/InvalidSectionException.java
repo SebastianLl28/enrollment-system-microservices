100909f06fc03b61b7ba13016b519c9e23c7c37a
Alonso <llamucasebas@gmail.com>
2026-07-11 10:20:01 -0500

    feat: add InvalidSectionException for handling invalid section errors

---
+++ enrollment-server/src/main/java/com/app/enrollment/system/enrollment/server/domain/exception/InvalidSectionException.java
@@ -0,0 +1,11 @@
+package com.app.enrollment.system.enrollment.server.domain.exception;
+
+/**
+ * @author Alonso
+ */
+public class InvalidSectionException extends RuntimeException {
+
+  public InvalidSectionException(String message) {
+    super(message);
+  }
+}
